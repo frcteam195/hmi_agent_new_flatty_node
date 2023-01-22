556a031fcd58c8ed161f39c78647a4508be151c0
Mac2 <team195programming@gmail.com>
2023-01-22 21:50:46 +0000

    LED Control Logic

--- src/hmi_agent_node/main.py
+++ src/hmi_agent_node/main.py
@@ -6,7 +6,7 @@ from ck_ros_base_msgs_node.msg import Joystick_Status, Robot_Status
 from ck_ros_msgs_node.msg import HMI_Signals
 from ck_utilities_py_node.joystick import Joystick
 from ck_utilities_py_node.ckmath import *
-from ck_ros_msgs_node.msg import Intake_Control
+from ck_ros_msgs_node.msg import Intake_Control, Led_Control
 from nav_msgs.msg._Odometry import Odometry
 import numpy as np
 
@@ -43,24 +43,26 @@ operator_params = OperatorParams()
 hmi_pub = None
 odom_pub = None
 intake_pub = None
+led_control_pub = None
 
 drive_joystick = Joystick(0)
 operator_controller = Joystick(1)
 
-is_auto = False
+robot_state = Robot_Status.DISABLED
 
 drivetrain_orientation = HMI_Signals.ROBOT_ORIENTED
 
 
 def robot_status_callback(msg: Robot_Status):
-    global is_auto
-    is_auto = (msg.robot_state == msg.AUTONOMOUS)
+    global robot_state
+    robot_state = msg.robot_state
 
 
 def joystick_callback(msg: Joystick_Status):
     global drivetrain_orientation
-    global is_auto
+    global robot_state
     global hmi_pub
+    global led_control_pub
 
     global drive_joystick
     global operator_params
@@ -115,7 +117,49 @@ def joystick_callback(msg: Joystick_Status):
     hmi_update_msg.drivetrain_orientation = drivetrain_orientation
 
     # LED Control
-    # TODO
+    led_control_msg = Led_Control()
+    if robot_state == Robot_Status.DISABLED:
+        #Red SOS
+        led_control_msg.control_mode = Led_Control.SET_LED
+        led_control_msg.red = 255
+        led_control_msg.green = 0
+        led_control_msg.blue = 0
+        led_control_msg.white = 0
+        led_control_msg.number_leds = 8
+    else:
+        if operator_controller.getButton(operator_params.cone_request_button_id):
+            #cone
+            led_control_msg.control_mode = Led_Control.SET_LED
+            led_control_msg.red = 255
+            led_control_msg.green = 255
+            led_control_msg.blue = 0
+            led_control_msg.white = 0
+            led_control_msg.number_leds = 8
+        elif operator_controller.getButton(operator_params.cube_request_button_id):
+            #cube
+            led_control_msg.control_mode = Led_Control.SET_LED
+            led_control_msg.red = 255
+            led_control_msg.green = 0
+            led_control_msg.blue = 255
+            led_control_msg.white = 0
+            led_control_msg.number_leds = 8
+        elif operator_controller.getButton(operator_params.party_mode_button_id):
+            #party
+            led_control_msg.control_mode = Led_Control.SET_LED
+            led_control_msg.red = 0
+            led_control_msg.green = 255
+            led_control_msg.blue = 0
+            led_control_msg.white = 0
+            led_control_msg.number_leds = 8
+        else:
+            led_control_msg.control_mode = Led_Control.SET_LED
+            led_control_msg.red = 255
+            led_control_msg.green = 0
+            led_control_msg.blue = 255
+            led_control_msg.white = 0
+            led_control_msg.number_leds = 8
+
+    led_control_pub.publish(led_control_msg)
 
     if drive_joystick.getButton(drive_params.reset_odometry_button_id):
         odom = Odometry()
@@ -253,12 +297,14 @@ def ros_main(node_name):
     global hmi_pub
     global odom_pub
     global intake_pub
+    global led_control_pub
     rospy.init_node(node_name)
     init_params()
 
     hmi_pub = rospy.Publisher(name="/HMISignals", data_class=HMI_Signals, queue_size=10, tcp_nodelay=True)
     odom_pub = rospy.Publisher(name="/ResetHeading", data_class=Odometry, queue_size=10, tcp_nodelay=True)
     intake_pub = rospy.Publisher(name="/IntakeControl", data_class=Intake_Control, queue_size=10, tcp_nodelay=True)
+    led_control_pub = rospy.Publisher(name="/LedControl", data_class=Led_Control, queue_size=10, tcp_nodelay=True)
 
     rospy.Subscriber(name="/JoystickStatus", data_class=Joystick_Status, callback=joystick_callback, queue_size=1, tcp_nodelay=True)
     rospy.Subscriber(name="/RobotStatus", data_class=Robot_Status, callback=robot_status_callback, queue_size=1, tcp_nodelay=True)
